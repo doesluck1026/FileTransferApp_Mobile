193b17f23710341d9641b31ffe0508f2b5bbed3c
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2021-03-11 21:08:10 +0300

    Adding language options.

--- FileTransferApp_Mobile/FileTransferApp_Mobile/Resources/AppResources.Designer.cs
+++ FileTransferApp_Mobile/FileTransferApp_Mobile/Resources/AppResources.Designer.cs
@@ -22,14 +22,14 @@ namespace FileTransferApp_Mobile.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class LanguageResource {
+    internal class AppResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal LanguageResource() {
+        internal AppResources() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace FileTransferApp_Mobile.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileTransferApp_Mobile.Resources.LanguageResource", typeof(LanguageResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileTransferApp_Mobile.Resources.AppResources", typeof(AppResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
