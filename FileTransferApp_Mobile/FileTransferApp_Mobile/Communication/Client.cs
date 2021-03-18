5085106bb4840dd223ef97bef1b5cc687a945616
doesluck1026 <58068335+doesluck1026@users.noreply.github.com>
2021-03-19 00:12:18 +0300

    bugs fixed

--- FileTransferApp_Mobile/FileTransferApp_Mobile/Communication/Client.cs
+++ FileTransferApp_Mobile/FileTransferApp_Mobile/Communication/Client.cs
@@ -51,8 +51,8 @@ class Client
             IsConnectedToServer = true;
             client.SendBufferSize = BufferSize;
             client.ReceiveBufferSize = BufferSize;
-            Debug.WriteLine("Succesfully Connected to: " + IP + " on Port: " + Port);
-            if (timeout == 0)
+            Debug.WriteLine("Succesfully Connected to: " + IP + " on Port: " + Port + " time: " + stp.Elapsed.TotalMilliseconds + " ms");
+            if (timeout > 0)
             {
                 var host = Dns.GetHostEntry(IP);
                 return host.HostName;
